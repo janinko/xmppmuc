b8c7999e1bb25c2be839a9859d4fbcaef87b39ab
Honza Brázdil <janinko.g@gmail.com>
2012-07-31 13:09:16 +0200

    forgeted import

--- src/main/java/eu/janinko/xmppmuc/commands/ConfigManager.java
+++ src/main/java/eu/janinko/xmppmuc/commands/ConfigManager.java
@@ -20,6 +20,7 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+import org.apache.log4j.Logger;
 
 public class ConfigManager {
 	Document doc;
